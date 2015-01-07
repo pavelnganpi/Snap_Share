0732f69914ccc6d0eb87e8d1687a2c834b8dc68f
paveytel <paveytel@buffalo.edu>
2015-01-07 17:38:44 -0500

    list of messages in InboxFragment.java get updated when a message is deleted.

--- app/src/main/java/com/paveynganpi/snapshare/InboxFragment.java
+++ app/src/main/java/com/paveynganpi/snapshare/InboxFragment.java
@@ -18,6 +18,7 @@ import com.parse.ParseObject;
 import com.parse.ParseQuery;
 import com.parse.ParseUser;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -66,6 +67,24 @@ public class InboxFragment extends ListFragment {
 
                 }
                 //delete
+                List<String> ids = message.getList(ParseConstants.KEY_RECIPIENT_IDS);
+                if(ids.size() == 1){
+                    message.deleteInBackground();
+
+
+                }
+                else{
+                    //remove the recipient and save
+                    ids.remove(ParseUser.getCurrentUser().getObjectId());
+
+                    ArrayList<String> idsToRemove = new ArrayList<String>();
+                    idsToRemove.add(ParseUser.getCurrentUser().getObjectId());
+
+                    message.removeAll(ParseConstants.KEY_RECIPIENT_IDS,idsToRemove);
+                    message.saveInBackground();
+
+
+                }
 
 
             }
