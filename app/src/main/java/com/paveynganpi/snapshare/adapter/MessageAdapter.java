6785214ccebda6c1936e843a146f0af215858d6c
paveytel <paveytel@buffalo.edu>
2015-01-15 17:45:03 -0500

    styled the message_item.xml with designer mockups

--- app/src/main/java/com/paveynganpi/snapshare/adapter/MessageAdapter.java
+++ app/src/main/java/com/paveynganpi/snapshare/adapter/MessageAdapter.java
@@ -55,10 +55,10 @@ public class MessageAdapter extends ArrayAdapter {
 
         ParseObject message = mMessages.get(position);
         if(message.getString(ParseConstants.KEY_FILE_TYPE).equals(ParseConstants.TYPE_IMAGE)) {
-            holder.iconImageView.setImageResource(R.drawable.ic_action_picture);
+            holder.iconImageView.setImageResource(R.drawable.ic_picture);
         }
         else {
-            holder.iconImageView.setImageResource(R.drawable.ic_action_play_over_video);
+            holder.iconImageView.setImageResource(R.drawable.ic_video);
         }
         holder.nameLabel.setText(message.getString(ParseConstants.KEY_SENDER_NAME));
 
